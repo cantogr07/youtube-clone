ae10d36990e3bc5ec69dda2c0cf6f36e11334693
cantogr07 <cantogran2007@gmail.com>
2023-07-07 17:49:35 +0300

    edited .env to match with vite requirements-seems to be working

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Stack, Box } from '@mui/material';
 
 const Videos = ({ videos }) => {
-  return (
-    <div>Videos</div>
-  )
+    return (
+        <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+            {videos.map}
+
+        </Stack>
+    )
 }
+  
+
 
 export default Videos
